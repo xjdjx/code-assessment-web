580d2712e2ce9aebecaaebd63eff35a6921bf1f3
James De Jesus <xjamesdejesusx@gmail.com>
2017-12-02 18:51:28 -0800

    switching to remote endpoint, remapping json

--- src/api/shop.js
+++ src/api/shop.js
@@ -1,11 +1,20 @@
-/**
- * Mocking client-server processing
- */
-import _products from './products.json'
-
 const TIMEOUT = 100
 
+const remapJson = json => {
+	json.forEach(obj => {
+		obj['title'] = obj['productTitle']
+		delete obj['productTitle']
+		obj['price'] = obj['price']['value']
+	})
+
+	return json
+}
+
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: (cb) => {
+  	return fetch(`http://tech.work.co/shopping-cart/products.json`)
+      .then(response => response.json())
+      .then(json => cb(remapJson(json)))
+  },
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
