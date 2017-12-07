fc79eb6c8d2e359f46a7a02b935478cb9d051fb5
James De Jesus <xjamesdejesusx@gmail.com>
2017-12-06 21:48:35 -0800

    fix cart quantity

--- src/components/CartStatus.js
+++ src/components/CartStatus.js
@@ -4,8 +4,13 @@ import { getCartProducts } from '../reducers'
 import { openCart } from '../actions'
 
 const CartStatus = ({products, openCart}) => {
-	const plural = products.length > 1 ? 's' : ''
-	const message = products.length > 0 ? `You have ${products.length} item${plural} in your cart` : 'Your cart is empty'
+	let product_count = 0
+	products.forEach ((product) => {
+		product_count += product.quantity
+	});
+
+	const plural = product_count > 1 ? 's' : ''
+	const message = product_count > 0 ? `You have ${product_count} item${plural} in your cart` : 'Your cart is empty'
 
 	return (
 		<div className='cartStatus' onClick={openCart}>
